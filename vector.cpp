48b8ca7b476fcd0bb02cab660189e800af14ce5b
ingeniousname <miloszwozny5@gmail.com>
2022-04-13 17:05:58 +0200

    implementacja wektora

--- vector.cpp
+++ vector.cpp
@@ -1,4 +1,64 @@
-//
-// Created by milosz on 13.04.2022.
-//
+#include "vector.h"
+
+Vector2 Vector2::operator+(const Vector2& other) {
+    return {this->x + other.x, this->y + other.y};
+}
+
+Vector2 & Vector2::operator+=(const Vector2& other) {
+    this->x += other.x;
+    this->y += other.y;
+    return *this;
+}
+
+Vector2 Vector2::operator-(const Vector2& other) {
+    return {this->x - other.x, this->y - other.y};
+}
+
+Vector2 & Vector2::operator-=(const Vector2& other) {
+    this->x -= other.x;
+    this->y -= other.y;
+    return *this;
+}
+
+Vector2 Vector2::operator*(const double& scalar) {
+    return {this->x * scalar, this->y * scalar};
+}
+
+Vector2 & Vector2::operator*=(const double& scalar) {
+    this->x *= scalar;
+    this->y *= scalar;
+    return *this;
+}
+
+Vector2 Vector2::operator/(const double& scalar) {
+    return {this->x / scalar, this->y / scalar};
+}
+
+Vector2 & Vector2::operator/=(const double& scalar) {
+    this->x /= scalar;
+    this->y /= scalar;
+    return *this;
+}
+
+Vector2 operator*(const double& scalar, const Vector2& v){
+    return {v.x * scalar, v.y * scalar};
+}
+
+double dot_product(const Vector2 &v1, const Vector2 &v2) {
+    return v1.x * v2.x + v1.y * v2.y;
+}
+
+double Vector2::mag() const{
+    return std::sqrt(this->mag_squared());
+}
+
+double Vector2::mag_squared() const {
+    return this->x * this->x + this->y * this->y;
+}
+
+void Vector2::normalize() {
+    double length = this->mag();
+    this->x /= length;
+    this->y /= length;
+}
 
--- vector.h
+++ vector.h
@@ -1,8 +1,44 @@
-//
-// Created by milosz on 13.04.2022.
-//
+#ifndef VECTOR_H_
+#define VECTOR_H_
 
-#ifndef ANDREEV_KNFK_VECTOR_H
-#define ANDREEV_KNFK_VECTOR_H
+#include <cmath>
+#include <initializer_list>
 
-#endif //ANDREEV_KNFK_VECTOR_H
+
+
+
+class Vector2
+{
+    public:
+    double x, y;
+
+    Vector2() {this->x = 0; this->y = 0; };
+    Vector2(double x_, double y_) {this->x = x_; this->y = y_; };
+    Vector2(const Vector2& other) {this->x = other.x; this->y = other.y; };
+
+
+    Vector2 operator+(const Vector2& other);
+    Vector2 & operator+=(const Vector2& other);
+    Vector2 operator-(const Vector2& other);
+    Vector2 & operator-=(const Vector2& other);
+    Vector2 operator*(const double& scalar);
+    Vector2 & operator*=(const double& scalar);
+    Vector2 operator/(const double& scalar);
+    Vector2 & operator/=(const double& scalar);
+
+    friend Vector2 operator*(const double& scalar, const Vector2& v);
+
+
+    double mag() const;
+    double mag_squared() const;
+    void normalize();
+
+};
+
+double dot_product(const Vector2& v1, const Vector2& v2);
+
+
+
+
+
+#endif
