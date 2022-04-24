de2dd3eade724b06a507ea001ea762fcea892050
Maciej Standerski <01159859@pw.edu.pl>
2022-04-24 21:12:38 +0200

    Funkcja do zapisu polozen czastek

--- print.cpp
+++ print.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void print_to_file(char* filename, double time, int N_z[], int no_slices, float length) {
+void print_N_to_file(char* filename, double time, int N_z[], int no_slices, float length) {
 	//sprawdzenie czy plik jest pusty
 	bool file_header = true;
 	ifstream input_file;
@@ -35,6 +35,38 @@ void print_to_file(char* filename, double time, int N_z[], int no_slices, float 
 	output_file << time << ",";
 	for(int i = 0; i < no_slices; i++) {
 		output_file << N_z[i] << ",";
+	}
+	output_file << endl;
+	output_file.close();
+}
+
+void print_positions_to_file(char* filename, double time, double z[], double r[], int no_particles) {
+	//sprawdzenie czy plik jest pusty
+	bool file_header = true;
+	ifstream input_file;
+	input_file.open(filename);
+	if(input_file.peek() == ifstream::traits_type::eof())
+		file_header = false;
+	input_file.close();
+
+	//zapis do pliku
+	ofstream output_file;
+	output_file.open(filename, std::ofstream::out | std::ofstream::app);
+	
+	//jesli plik pusty, stworz naglowek
+	if(!file_header) {
+		output_file << "t [s]" << ",";
+		for(int i = 0; i < no_particles; i++) {
+			output_file << "z" << i+1 << "," << "r" << i+1 << ",";
+		}
+		output_file << endl;
+		file_header = true;
+	}
+
+	//wypisz dane do pliku
+	output_file << time << ",";
+	for(int i = 0; i < no_particles; i++) {
+		output_file << z[i] << "," << r[i] << ",";
 	}
 	output_file << endl;
 	output_file.close();
--- print.h
+++ print.h
@@ -1,11 +1,16 @@
 #ifndef _PRINT_H_
 #define _PRINT_H_
 
-//zapisywanie do pliku stanu ukladu w danej chwili czasu
-void print_to_file(char* filename, double time, int N_z[], int no_slices, float lenght);
+//zapisywanie do pliku liczby czastek w poszczegolnych kawalkach torusa w danej chwili czasu
+void print_N_to_file(char* filename, double time, int N_z[], int no_slices, float lenght);
 //filename - nazwa pliku (z rozszerzeniem .csv)
 //N_z[] - tablica zawierajaca liczbe czastek w poszczegolnych binach
 //no_slices - liczba binow (kawalkow, na ktore podzielony jest torus)
 //lenght - dlugosc torusa
+
+//zapisywanie do pliku polozen czastek w danej chwili czasu
+void print_positions_to_file(char* filename, double time, double z[], double r[], int no_particles);
+//z - pozycje z
+//r - pozycje r
 
 #endif
