4368ca5cc6feae08053858dbf5e5a8ed7b028cf2
Bartek14 <bruszczak48@gmail.com>
2022-04-27 17:16:13 +0200

    Dodanie pierwotnej funkcji z prostym algorytmem symulacji

---
+++ simulation.cpp
@@ -0,0 +1,27 @@
+#include "particle.h"
+#include "collision.h"
+void verlet(Particle *particles, int count, Variables params){
+    int timestep=1;//musi być albo zafiksowane albo czytane z pliku inputowego
+    int stepsMade=0;
+    int simSteps=100;//musi być albo zafiksowane albo czytane z pliku inputowego
+    //pętla symulacji
+    while (stepsMade<=simSteps)
+    {
+        //pętla po wszystkich cząstkach
+        for (int idx = 0; idx < count; idx++)
+        {   
+            //sprawdzanie czy cząstka ma kolizję ze ścianą
+            if (collision_detection(particles[idx], params.width, params.lenght)){
+                andreev_reflection(particles[idx], params.width, params.Et, timestep);
+            }
+            else{
+                //zalozenie ze mass=1
+                // particles[idx].pos_R = particles[idx].pos_R + particles[idx].momentum_r*timestep/particles[idx].mass;
+                particles[idx].pos_R += particles[idx].momentum_r*timestep;
+                // particles[idx].pos_Z = particles[idx].pos_Z + particles[idx].momentum_z*timestep/particles[idx].mass;
+                particles[idx].pos_Z += particles[idx].momentum_z*timestep;
+            }   
+        }
+        stepsMade++;   
+    }    
+}
