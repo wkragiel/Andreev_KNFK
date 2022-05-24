5b31ad8d5127afd185c86e316811dbc837fc3033
wkragiel <01150705@pw.edu.pl>
2022-05-24 22:42:03 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "vector.h"
+#include "input.h"
+#include "collision.h"
+#include "particle.h"
+#include "print.h"
+#include <iostream>
+
+int main(int argc, char* argv[]){
+
+
+
+    return 0;
+}
